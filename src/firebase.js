cf3a29c7c90cab16df66e1f82c0269f67ed15336
Mohammed Shoaib M <145997638+MaDycloud-MD@users.noreply.github.com>
2025-06-22 17:41:55 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // Your Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDWdKQjkf6Rh_o72k7zJA7DHRIlAf5SP_M",
+  apiKey: "--------------------------",
   authDomain: "smart-city-ea357.firebaseapp.com",
   projectId: "smart-city-ea357",
   storageBucket: "smart-city-ea357.appspot.com",
-  messagingSenderId: "232623747138",  // ✅ Real value
-  appId: "1:232623747138:web:ee422244b80ed817a4fddb",  // ✅ Real value
-  measurementId: "G-1ABCDEF2G"  // ✅ Real value
+  messagingSenderId: "----------",  // Real value
+  appId: "-------------------",  // Real value
+  measurementId: "----------------"  // Real value
 };
 
 // Initialize Firebase
